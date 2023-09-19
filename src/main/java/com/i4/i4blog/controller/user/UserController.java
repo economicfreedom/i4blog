8e7d4c8f879e2a57b3bb240d6c15b39f402e9f90
pys <bark6256@naver.com>
2023-09-19 16:51:53 +0900

    img 경로 수정, 로그인 페이지 이동 테스트

--- src/main/java/com/i4/i4blog/controller/user/UserController.java
+++ src/main/java/com/i4/i4blog/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.i4.i4blog.controller.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
+	// PYS
+	
+	/**
+	 * 로그인 페이지로 이동
+	 */
+	@GetMapping("/sign-in")
+	public String signin() {
+		return "user/signIn";
+	}
 }
