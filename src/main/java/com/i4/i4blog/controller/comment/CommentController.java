2a24dcfeb3f20e3d3f7a8cc4f3deeb8bbb596477
mariostar2 <alskfl7777@naver.com>
2023-09-27 17:59:51 +0900

    댓글 생성 진행중

--- src/main/java/com/i4/i4blog/controller/comment/CommentController.java
+++ src/main/java/com/i4/i4blog/controller/comment/CommentController.java
@@ -2,13 +2,18 @@ package com.i4.i4blog.controller.comment;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.i4.i4blog.dto.comment.Commentwritedto;
+import com.i4.i4blog.service.comment.CommentService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,26 +24,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CommentController {
 	
+	
+	
 	/** 
 	 *
-	 * 댓글 작성 페이지  
-	 
+	 * 댓글 작성 페이지 요청
 	 */
 	@GetMapping("/create")
-	public String commentCreate(@ModelAttribute Commentwritedto dto , HttpSession session) {
-		return "comment/commentcreate";
+	public String commentWrite(@ModelAttribute Commentwritedto dto , HttpSession session) {
+		return "comment/commentCreate";
 	}
-	
 	
 	/**
 	 * 
-	 * 댓글 수정 페이지
+	 * 댓글 수정 페이지 요청
 	 */
 	@PutMapping("/put")
 	public String commentPut() {
 		return "put" ;
 	}
 	
-	//댓글 삭제 페이지 이동
+	
+	
+	/**
+	 * 댓글 작성 기능 
+	 */ 
+//	@RequestMapping(value ="")
+//	public String createComment() {
+//		
+//	}
+
+	
 	
 }
--- src/main/java/com/i4/i4blog/repository/interfaces/comment/CommentRepository.java
+++ src/main/java/com/i4/i4blog/repository/interfaces/comment/CommentRepository.java
@@ -1,15 +1,19 @@
 package com.i4.i4blog.repository.interfaces.comment;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.i4.i4blog.repository.model.comment.Comment;
-
-
  
 @Mapper
 public interface CommentRepository {
-	public Comment commentById (String comment);
 	
+	/**
+	 * 
+	 * @param comment
+	 * @return
+	 */
 	public int insert(Comment comment);
+
+	
+	
 	
 }
