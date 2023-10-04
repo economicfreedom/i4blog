d22136e4199ecc8bb1f8665095717f1102745495
yj <dpwjd304@naver.com>
2023-10-04 17:30:47 +0900

    카테고리 헤더 오류 수정

--- src/main/java/com/i4/i4blog/controller/user/UserController.java
+++ src/main/java/com/i4/i4blog/controller/user/UserController.java
@@ -1,17 +1,13 @@
 package com.i4.i4blog.controller.user;
 
 import java.security.Principal;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.i4.i4blog.dto.user.UserJoinFormDto;
-import com.i4.i4blog.repository.model.category.Category;
-import com.i4.i4blog.service.category.CategoryService;
 import com.i4.i4blog.service.user.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,22 +18,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
-    // PYS
+    // 박용세
     private final UserService userService;
 
-    // 테스트용
-    private final CategoryService categoryService;
-
     /**
-     * 로그인 페이지로 이동
+     * 로그인 페이지 이동
      */
     @GetMapping("/login")
     public String login() {
         return "user/login";
     }
 
+    @GetMapping("/success")
+    public String loginSuccess(Principal principal) {
+       return "redirect:/blog/"+principal.getName();
+    }
+    
     /**
-     * 로그인 페이지 이동
+     * 회원가입 페이지 이동
      */
     @GetMapping("/join")
     public String join() {
@@ -53,15 +51,7 @@ public class UserController {
     @PostMapping("/join")
     public String joinProc(UserJoinFormDto userJoinFormDto) {
         userService.userJoinService(userJoinFormDto);
-        return "redirect:/user/login";
+        return "redirect:/blog/" + userJoinFormDto.getUserId();
     }
-    
-    @GetMapping("/test")
-    public String userTest(Model model) {
-    	List<Category> categoryList = categoryService.findByUserId(1);
-    	model.addAttribute("categoryList", categoryList);
-    	return "category-temp";
-    }
-
 
 }
