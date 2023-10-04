76d2f0689aa35f9d949dd27e7ad5a2182d8129d9
pys <bark6256@naver.com>
2023-10-04 16:54:48 +0900

    카테고리 인터셉터 적용 및 로그인 redirect 설정. 테스트 반드시 필요

---
+++ src/main/java/com/i4/i4blog/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.i4.i4blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private BlogInterceptor blogInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(blogInterceptor)
+			.addPathPatterns("/blog/{userId}/**")
+			.excludePathPatterns("/admin/**", "/user/login", "/user/join"
+					, "/js/**", "/css/**", "/img/**");
+	}
+}
--- src/main/webapp/WEB-INF/view/category-temp.jsp
+++ src/main/webapp/WEB-INF/view/category-temp.jsp
@@ -1,27 +1,4 @@
 <%@ page contentType="text/html; charset=UTF-8" language="java" pageEncoding="UTF-8" %>
 <%@ include file="/WEB-INF/view/layout/category-header.jsp"%>
 
-111
-<br>
-111222
-<br>
-111333
-<br>
-111444
-<br>
-111555
-<br>
-111666
-<br>
-111777
-<br>
-111888
-<br>
-11199999
-<br>
-1110000000
-<br>
-111
-<br>
-2222222
 <%@ include file="/WEB-INF/view/layout/category-footer.jsp"%>
