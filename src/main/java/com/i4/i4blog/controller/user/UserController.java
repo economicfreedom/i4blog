d6f743991d23ba41e01115f77769272d7d260463
pys <bark6256@naver.com>
2023-10-06 12:47:24 +0900

    카테고리 관리 레이아웃 진행중

--- src/main/java/com/i4/i4blog/controller/user/UserController.java
+++ src/main/java/com/i4/i4blog/controller/user/UserController.java
@@ -1,13 +1,17 @@
 package com.i4.i4blog.controller.user;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.i4.i4blog.dto.user.UserJoinFormDto;
+import com.i4.i4blog.repository.model.category.Category;
+import com.i4.i4blog.service.category.CategoryService;
 import com.i4.i4blog.service.user.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +26,8 @@ public class UserController {
      * @author 박용세
      */
     private final UserService userService;
+    // 테스트
+    private final CategoryService categoryService;
 
     /**
      * 로그인 페이지 이동
@@ -61,5 +67,11 @@ public class UserController {
         return "redirect:/user/login";
     }
 
-
+    // 카테고리 테스트중
+    @GetMapping("/category-setting")
+    public String categorySetting(Model model) {
+    	List<Category> categoryList = categoryService.findByUserId("aaaa");
+    	model.addAttribute(categoryList);
+    	return "user/categorySetting";
+    }
 }
--- src/main/java/com/i4/i4blog/repository/interfaces/user/UserRepository.java
+++ src/main/java/com/i4/i4blog/repository/interfaces/user/UserRepository.java
@@ -3,10 +3,11 @@ package com.i4.i4blog.repository.interfaces.user;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.i4.i4blog.repository.model.user.User;
+import com.i4.i4blog.vo.user.ProfileInfoVo;
 
 @Mapper
 public interface UserRepository {
 	public User findByUserId(String userId);
-
+	public ProfileInfoVo findProfileByUserId(String userId);
 	public int insert(User user);
 }
